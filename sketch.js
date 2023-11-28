2f7465fea9682eafd8a009130190ae3076b02533
EmilyGraceLB <egb556@york.ac.uk>
2023-11-28 20:45:17 +0000

    IT WORKS
    i'm a genius omg slay

--- sketch.js
+++ sketch.js
@@ -7,13 +7,13 @@ let longMarket;
 let marketWithTrees;
 let christmasTree;
 let marketMixedLights;
+let check;
 
 let buildingSelection = [];
 //stores images
 
 let buildingObject = [];
 //stores building objects
-
 
 function preload(){
     spriteImg = loadImage("assets/Player 1 sprite CC (3).png");
@@ -24,8 +24,6 @@ function preload(){
     buildingSelection [2] = loadImage("assets/chrima tree 100 100 copy.png");
     buildingSelection [3] = loadImage("assets/market mixed lights.png");
 }
-
-
 
 class sprite {
     spriteY;
@@ -72,14 +70,15 @@ class building {
     constructor(){
         this.topBuildingX = 600;
         this.topBuildingY = random(50,300);
-        this.topBuildingW = 200;
+        // this.topBuildingW = 200;
         // this.topBuildingH = random(50,450);
 
         this.speedBuilding = 1;
 
         this.botBuildingX = 600;
         this.botBuildingY = random(300,400);
-        this.botBuildingW = 200;        
+        // this.botBuildingW = 200;
+        // this.botBuildingH = random(50,300);      
     }
     drawbuilding(){
             image(this.selectedImage, this.topBuildingX, this.topBuildingY);
@@ -90,38 +89,38 @@ class building {
         this.topBuildingX += - this.speedBuilding;
         this.botBuildingX += - this.speedBuilding;
     }
+
     selectImage(){
         this.selectedImage = random(buildingSelection);
     }
+
 }
-
-
 
 function setup(){
     createCanvas(600,600);
     sprite1 = new sprite();
-
     buildingObject.push(new building());
-    
+    buildingObject[0].selectImage();
+
 }
 
 function draw(){
     background(255);
     image(backgroundGravel,0,0);
-
-    sprite1.show();
-    keyPressed();
-    // sprite1.spriteYDown();
-    building1.drawbuilding();
-    building1.buildingMove();
-    if(frameCount % 50 == 0){
-        buildingObject.push(new building());
-    }
-    for(i =0; i < buildingObject.length; i++){
-        buildingObject[i].selectImage();
+    for(let i = buildingObject.length -1; i >= 0; i--){
+        
         buildingObject[i].drawbuilding();
         buildingObject[i].buildingMove();
     }
+    sprite1.show();
+    keyPressed();
+    if (frameCount % 350 == 0) {
+            building1 = new building();
+            building1.selectImage();
+            buildingObject.push(building1);
+    }
+    // sprite1.spriteYDown();
+
 }
 
 function keyPressed(){
